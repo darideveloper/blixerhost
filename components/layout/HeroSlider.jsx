20c1b3ca37ddf974ab10d5c49462e4825c235436
daridev <darideveloper@gmail.com>
2024-12-18 18:42:59 -0600

    feat (images): add solid bg

--- components/layout/HeroSlider.jsx
+++ components/layout/HeroSlider.jsx
@@ -73,7 +73,6 @@ export default function HeroSlider() {
                 flex
                 items-center
                 justify-center
-                opacity-10
                 overflow-visible
               `}
             >
